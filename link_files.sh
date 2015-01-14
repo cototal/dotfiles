2a29ec66a517bfeab9b28d2e7ef93a8b5604ec0c
Shaun Martin <martins@surgery.wisc.edu>
2015-01-14 13:24:06 -0600

    Correctly link files, bundle exec rails command.

--- link_files.sh
+++ link_files.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-rm ~/.bashrc
-rm ~/.bash_aliases
-rm ~/.bash_rails
-rm ~/.gemrc
+cd
 
-ln -s .bashrc ~/.bashrc
-ln -s .bash_aliases ~/.bash_aliases
-ln -s .bash_rails ~/.bash_rails
-ln -s .gemrc ~/.gemrc
+rm .bashrc
+rm .bash_aliases
+rm .bash_rails
+rm .gemrc
+
+ln -s dotfiles/.bashrc
+ln -s dotfiles/.bash_aliases
+ln -s dotfiles/.bash_rails
+ln -s dotfiles/.gemrc
+
+cd dotfiles
