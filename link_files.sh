8a4bb468f45b75221a2eceb42b1ec49b90e28b68
Shaun Martin <webmaster@ginblades.com>
2015-07-14 17:49:31 +0000

    Add bash shared file.

--- link_files.sh
+++ link_files.sh
@@ -2,10 +2,12 @@
 
 cd
 
+rm .bash_shared
 rm .bash_aliases
 rm .bash_rails
 rm .gemrc
 
+ln -s dotfiles/.bash_shared
 ln -s dotfiles/.bash_aliases
 ln -s dotfiles/.bash_rails
 ln -s dotfiles/.gemrc
